4ce6fd272352212fdcca4be9e9b077fb37bbfcb5
Jethro <jethrotiago@hotmail.com>
2023-10-02 00:42:06 -0300

    Created logic to add and remove the class selecionado in buttons and images

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,22 @@
-console.log("Aplicação desenvolvido por Jethro Tiago");
+const personagens = document.querySelectorAll(".personagem");
+const botoes = document.querySelectorAll(".botao");
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        desselecionarBotao();
+        desselecionarPersonagem();
+        
+        botao.classList.add("selecionado");
+        personagens[indice].classList.add("selecionado");
+    })
+})
+
+function desselecionarPersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
